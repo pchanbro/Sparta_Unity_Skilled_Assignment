20e47501caebae0ab4308b65c088d86347889179
pchanbro <cksgud0509@naver.com>
2024-10-29 21:58:31 +0900

    Feat : Add JumpBox
    OnCollisionEnter가 발생하면 점프!

---
+++ SpartaDungeon/Assets/Scripts/JumpingBox.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingBox : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.rigidbody.AddForce(Vector2.up * 10 , ForceMode.Impulse);
+        Debug.Log("«Ǫ");
+    }
+}
